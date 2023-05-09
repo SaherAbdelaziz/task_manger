b5ac62c637010ff736ee469024136ccb087c91d3
SaherSayedAbdElaziz <saher.sayed@maxab.io>
2023-05-10 01:19:58 +0300

    basic files

---
+++ database/migrations/2023_05_09_221809_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('description');
+            $table->unsignedBigInteger('assigned_to_id');
+            $table->unsignedBigInteger('assigned_by_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
